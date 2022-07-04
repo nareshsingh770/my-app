8fe7081f217c8360c0195c85229bbfc50f9d69cc
Naresh Singh4 <nareshs4@chetu.com>
2022-07-04 17:32:11 +0530

    Header material implement

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
-import ColorsTheme from './ColorsTheme'
+import ColorsTheme from './ColorsTheme';
+import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem } from '@mui/material'
+import AdbIcon from '@mui/icons-material/Adb';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = (props) => {
 
@@ -46,6 +49,132 @@ const Header = (props) => {
                     <button className='btn btn-outline-warning text-warning logout-btn' onClick={props.userlogout}><i className="fa fa-power-off" aria-hidden="true"></i></button>
                 </div>
             </nav>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                <MenuItem >
+                                    <Typography textAlign="center">Home</Typography>
+                                </MenuItem>
+                                <MenuItem >
+                                    <Typography textAlign="center">Projects</Typography>
+                                </MenuItem>
+                                <MenuItem >
+                                    <Typography textAlign="center">Users</Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+                        <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                        <Typography
+                            variant="h5"
+                            noWrap
+                            component="a"
+                            href=""
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            LOGO
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+
+                            <Button
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                Home
+                            </Button>
+                            <Button
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                Projects
+                            </Button>
+                            <Button
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                Users
+                            </Button>
+
+                        </Box>
+
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Tooltip title="Open settings">
+                                <IconButton sx={{ p: 0 }}>
+                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                            >
+                                {/* {settings.map((setting) => (
+                                    <MenuItem key={setting}>
+                                        <Typography textAlign="center"></Typography>
+                                    </MenuItem>
+                                ))} */}
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
             <Outlet />
         </>
     )
--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
                 <div className="container">
                     <div className="text-heading">
                         <div className="text-1">Hello, My Name is</div>
-                        <div className="text-2">Naresh Singh</div>
+                        <div className="text-2">John Doe</div>
                         <div className="text-3">And I am a <span>Developer</span><span className="cursor">|</span></div>
                         <button type="button" className="btn btn-danger mt-2" onClick={() => setModal(!modalState)}>Hire me</button>
                     </div>
